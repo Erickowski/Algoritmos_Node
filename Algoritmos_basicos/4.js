27cabf96a2102baa6139fa72ae618646f6c27575
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 11:57:49 -0500

    Cuarto algoritmo

--- Algoritmos_basicos/4.js
+++ Algoritmos_basicos/4.js
@@ -1,2 +1,17 @@
 // Instrucciones
-//
+// Pedir dos números y decir si son iguales o no.
+
+let numeros = [];
+let indice = 0;
+
+console.log("Ingrese dos numeros");
+process.stdin.on("data", function (data) {
+  numeros.push(data.toString().trim());
+  indice++;
+  if (indice === 2) {
+    numeros[0] === numeros[1]
+      ? console.log("Los numeros son iguales")
+      : console.log("Los numeros no son iguales");
+    process.exit();
+  }
+});
