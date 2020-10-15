4fc339cb02676773bb959e3824288df3912ea71c
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 12:25:26 -0500

    Septimo algoritmo

--- Algoritmos_basicos/7.js
+++ Algoritmos_basicos/7.js
@@ -1,2 +1,21 @@
 // Instrucciones
-//
+// Pedir dos números y decir cual es el mayor.
+
+let numeros = [];
+let indice = 0;
+
+console.log("Ingrese dos numeros");
+process.stdin.on("data", function (data) {
+  numeros.push(data.toString().trim());
+  indice++;
+  if (indice === 2) {
+    if (numeros[0] > numeros[1]) {
+      console.log("El primer numero es mayor al segundo");
+    } else if (numeros[0] < numeros[1]) {
+      console.log("El segundo numero es mayor al primero");
+    } else {
+      console.log("Los numeros son iguales");
+    }
+    process.exit();
+  }
+});
