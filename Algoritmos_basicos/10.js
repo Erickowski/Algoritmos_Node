3ffcc273febbc7f3e9821454f54bcf544eccc6fa
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 12:28:53 -0500

    Decimo algoritmo

--- Algoritmos_basicos/10.js
+++ Algoritmos_basicos/10.js
@@ -1,2 +1,15 @@
 // Instrucciones
-//
+// Pedir tres números y mostrarlos ordenados de mayor a menor.
+
+let numeros = [];
+let indice = 0;
+
+console.log("Ingrese dos numeros");
+process.stdin.on("data", function (data) {
+  numeros.push(data.toString().trim());
+  indice++;
+  if (indice === 3) {
+    console.log(numeros.sort((a, b) => b - a));
+    process.exit();
+  }
+});
