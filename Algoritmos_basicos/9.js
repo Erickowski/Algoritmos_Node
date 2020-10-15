7fc7d21700495e9bd187a46bc3059e9f2f0d6564
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 12:28:11 -0500

    Noveno algoritmo

--- Algoritmos_basicos/9.js
+++ Algoritmos_basicos/9.js
@@ -1,2 +1,15 @@
 // Instrucciones
-//
+// Pedir dos números y mostrarlos ordenados de mayor a menor.
+
+let numeros = [];
+let indice = 0;
+
+console.log("Ingrese dos numeros");
+process.stdin.on("data", function (data) {
+  numeros.push(data.toString().trim());
+  indice++;
+  if (indice === 2) {
+    console.log(numeros.sort((a, b) => b - a));
+    process.exit();
+  }
+});
