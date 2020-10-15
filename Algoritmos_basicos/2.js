5e582ab6dee0f5060c1531f01593f1f6aa04b9ed
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 11:47:20 -0500

    Segundo algoritmo

--- Algoritmos_basicos/2.js
+++ Algoritmos_basicos/2.js
@@ -1,2 +1,11 @@
 // Instrucciones
-//
+// Pedir el radio de un círculo y calcular su área. A=PI*r^2.
+
+let radio;
+
+console.log("Ingrese el radio del circulo:");
+process.stdin.on("data", function (data) {
+  radio = data.toString().trim();
+  console.log(`El area del circulo es: ${Math.PI * Math.pow(radio, 2)}`);
+  process.exit();
+});
