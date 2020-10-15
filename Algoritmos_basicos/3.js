6a35f619d9b835daf6c3939eee6f4bcede222d6d
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 11:53:48 -0500

    Tercer algoritmo

--- Algoritmos_basicos/3.js
+++ Algoritmos_basicos/3.js
@@ -1,2 +1,11 @@
 // Instrucciones
-//
+// Pedir el radio de una circunferencia y calcular su longitud. L=2*PI*r.
+
+let radio;
+
+console.log("Ingrese el radio del circulo:");
+process.stdin.on("data", function (data) {
+  radio = data.toString().trim();
+  console.log(`La longitud del circulo es: ${2 * Math.PI * radio}`);
+  process.exit();
+});
