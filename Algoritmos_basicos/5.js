ea7b75ddea8c8890b578377f264d8d82e2f94e76
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 12:04:27 -0500

    Quinto  algoritmo

--- Algoritmos_basicos/5.js
+++ Algoritmos_basicos/5.js
@@ -1,2 +1,10 @@
 // Instrucciones
-//
+// Pedir un número e indicar si es positivo o negativo.
+
+console.log("Ingrese un numero");
+process.stdin.on("data", function (data) {
+  data.toString().trim() > 0
+    ? console.log("El numero es positivo")
+    : console.log("El numero es negativo");
+  process.exit();
+});
