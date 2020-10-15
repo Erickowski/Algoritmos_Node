2b8db52725cbdb4b28c69c24614f296534cc74d5
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 12:14:24 -0500

    Sexto algoritmo

--- Algoritmos_basicos/6.js
+++ Algoritmos_basicos/6.js
@@ -1,2 +1,23 @@
 // Instrucciones
-//
+// Pedir dos números y decir si uno es múltiplo del otro.
+
+let numeros = [];
+let indice = 0;
+
+console.log("Ingrese dos numeros");
+process.stdin.on("data", function (data) {
+  numeros.push(data.toString().trim());
+  indice++;
+  if (indice === 2) {
+    if (numeros[0] === numeros[1]) {
+      console.log("Ambos numeros son multiplos uno del otro.");
+    } else if (numeros[0] % numeros[1] === 0) {
+      console.log("El primer numero es multiplo del segundo");
+    } else if (numeros[1] % numeros[0] === 0) {
+      console.log("El segundo numero es multiplo del primer");
+    } else {
+      console.log("Ningun numero es multiplo del otro");
+    }
+    process.exit();
+  }
+});
