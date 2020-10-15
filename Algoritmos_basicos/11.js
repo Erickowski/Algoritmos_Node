34d44b0f2b804e01dbe5aae970d8d1c600b4ce98
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-15 12:33:21 -0500

    Onceavo algoritmo

--- Algoritmos_basicos/11.js
+++ Algoritmos_basicos/11.js
@@ -1,2 +1,17 @@
 // Instrucciones
-//
+// Pedir un número entre 0 y 9.999 y decir cuantas cifras tiene.
+
+let numero;
+
+console.log("Ingrese numero entre 0 y 9,999:");
+process.stdin.on("data", function (data) {
+  numero = data.toString().trim();
+  if (numero < 1 || numero > 9999) {
+    console.log("Numero no valido");
+  } else {
+    console.log(
+      "El numero " + numero + " tiene " + numero.length + " cifra(s)."
+    );
+  }
+  process.exit();
+});
